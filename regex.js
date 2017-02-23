6146e85907718ca3786af64afce49be268eee85d
Peter Wiggin <pwiggin7@gmail.com>
2017-02-23 17:14:51 -0600

    Reformatted braces, changed tabs to spaces

--- regex.js
+++ regex.js
@@ -1,11 +1,12 @@
 var example_text = "\nElection ResultsNationWorldOur Team\nTrump's Sweden comment raises questions\n\nBy Eric Bradner, CNN\nUpdated 2:24 PM ET, Sun February 19, 2017\n\n\nFlynn: One of the greatest presidencies\n\nJudge rejects request to delay travel ban case\n\nTrump defends ban, Trudeau has opposing view\n\nTrump shakes Japanese PM's hand for 19 seconds\n\nTapper: Trump's tweets a window into his soul\n\nTrump slams 'so-called' judge who halted ban\n\nCNN/ORC poll: Trump approval rating at 44%\n\nTrump's foreign policy sparks confusion\nNow Playing\nTrump's Sweden remark raises questions\n\nPresident Trump starts rally attacking media\n\nCNN anchor to Trump: Jefferson favored media\n\nTrump brings supporter on stage during rally\n\nCongress could force release of Trump's taxes\n\nTrump: I can live with 2 or 1-state solution\n\nTrump White House seeing chaotic first month\n\nWatch Trump ignore several questions on Russia\n\nFlynn: One of the greatest presidencies\n\nJudge rejects request to delay travel ban case\n\nTrump defends ban, Trudeau has opposing view\n\nTrump shakes Japanese PM's hand for 19 seconds\n\nTapper: Trump's tweets a window into his soul\n\nTrump slams 'so-called' judge who halted ban\n\nCNN/ORC poll: Trump approval rating at 44%\n\nTrump's foreign policy sparks confusion\n\nTrump's Sweden remark raises questions\n\nPresident Trump starts rally attacking media\n\nCNN anchor to Trump: Jefferson favored media\n\nTrump brings supporter on stage during rally\n\nCongress could force release of Trump's taxes\n\nTrump: I can live with 2 or 1-state solution\n\nTrump White House seeing chaotic first month\n\nWatch Trump ignore several questions on Russia\nStory highlights\n'We've got to keep our country safe,' he said\nThe White House did not immediately respond to a request for comment Sunday\nWashington (CNN)President Donald Trump's reference to 'what's happening last night in Sweden' during a Saturday rally in Florida raised questions in Sweden and around the internet about what he really meant.\n\nTrump referenced the Scandinavian nation, known for liberally accepting Syrian refugees, during a section of his speech decrying the dangers of open borders.\n'We've got to keep our country safe,' he said. 'You look at what's happening in Germany. You look at what's happening last night in Sweden. Sweden, who would believe this? Sweden. They took in large numbers. They're having problems like they never thought possible. You look at what's happening in Brussels. You look at what's happening all over the world. Take a look at Nice. Take a look at Paris.'\n\nKasich: World leaders 'just not sure' where Trump stands\nTrump appeared to be referring to recent terror attacks in Germany and elsewhere, but no such attack has occurred in Sweden. The White House did not immediately respond Sunday morning to questions about what Trump meant.\nThe official Twitter of the Embassy of Sweden in the US has responded to those asking about what happened Friday night by saying: 'Unclear to us what President Trump was referring to. Have asked US officials for explanation.'\nOthers on Twitter have speculated that Trump, who is a well-chronicled consumer of television news, might have been watching a segment on Fox News host Tucker Carlson's show Friday night.\nCarlson interviewed Ami Horowitz, a filmmaker who has tried to tie Sweden's taking in of asylum seekers to increased violent crimes in the country.\nCarl Bildt, the former Swedish prime minister, questioned the President's statement on Twitter.\n'Sweden? Terror attack? What has he been smoking?' Bildt tweeted. 'Questions abound.'\nTrump's remark is the latest misplaced reference to a terrorist attack or incident by those in his White House. Trump counselor Kellyanne Conway inaccurately referred to a 'Bowling Green massacre' that never took place, and White House press secretary Sean Spicer referred to an attack in Atlanta, later clarifying that he meant to refer to Orlando.\nSections\nElection ResultsNationWorldOur Team\nFollow us\nCNN.com\n© 2017 Cable News Network. Turner Broadcasting System, Inc. All Rights Reserved.\nCNN Sans ™ & © 2016 Cable News Network. Terms of service | Privacy guidelines\n\nOpinion: We need to reality-check ourselves on ICE raids\n\nSupporter: I salute cardboard Trump every day\n"
 
-
 var sentiment = require("sentiment")
 
-
-
-
+/*
+SCORES holds the information about all politicians in our dataset.
+Unfortunately, it must be defined above the functions in which it is used.
+Keep scrolling!
+*/
 
 var SCORES = [
 ['House', 'AK', 'R', 'Don', 'Young', '0.28', '', ''],
@@ -739,22 +740,21 @@ function get_article(full_text){
     for (i = 0; i < paragraph_array.length; i++){
         if (paragraph_array[i].length > min_paragraph_length){
             if (first_full_line == -1){
-            first_full_line = i
-            var consec_short_lines = 0 // start counter of non-article lines at 0
+                first_full_line = i
+                var consec_short_lines = 0 // start counter of non-article lines at 0
             } else {
-            consec_short_lines = 0 // reset counter if necessary 
+                consec_short_lines = 0 // reset counter if necessary 
             }
         } else if (first_full_line != -1){
             consec_short_lines += 1
             if (consec_short_lines == max_consec_short_lines){
-            last_line = i - max_consec_short_lines
-            }
+                last_line = i - max_consec_short_lines
             }
         }
+    }
     if (first_full_line == -1){
         return []
-    }
-    else if (last_line == -1){
+    } else if (last_line == -1){
         last_line = paragraph_array.length - 1
     }
     var article_array = paragraph_array.slice(first_full_line, last_line + 1)
@@ -764,14 +764,9 @@ function get_article(full_text){
 
 //console.log(get_article(example_text));
 
-/*
-SCORES holds the information about all politicians in our dataset.
-Unfortunately, it must be defined above the functions in which it is used.
-Keep scrolling!
-*/
 
 
-function find_names_in_article_mod(article_array){
+function find_names_in_article(article_array){
     /*
     Takes an array of paragraphs in an article.
     Returns a set of politicians found,
@@ -808,14 +803,13 @@ function find_names_in_article_mod(article_array){
                 if (is_name_in_string(text, combos[k][0] + " " + combos[k][1])){
                     politicians_in_article.add(SCORES[i]);
                     functioning_names.add([combos[k][0], combos[k][1]]);
-
                 }
             }
         }
-        };
-        // console.log(politicians_in_article);
-        let search_names = Array.from(functioning_names);
-        return [politicians_in_article, search_names];
+    };
+    // console.log(politicians_in_article);
+    let search_names = Array.from(functioning_names);
+    return [politicians_in_article, search_names];
     };
 
 
@@ -836,7 +830,7 @@ function get_sentences(article_array){
     for (i = 0; i < article_array.length; i++){
         var sentences_array = article_array[i].match(/[^\.!\?]+[\.!\?]+/g);
         if (sentences_array == null){
-            // No ., !. or ? in paragraph
+            // No ., !, or ? in paragraph
             sentences_array = [article_array[i]];
         }
         for (j = 0; j < sentences_array.length; j++){
@@ -856,12 +850,12 @@ function Politician(first, last, alternate){
     this.altname = alternate;
     this.sentiment = 0;
     this.statements = [];
-    };
+};
 
 
 
 
-//variable data here refers to the result you would get using the function find_names_in_article_mod
+//variable data here refers to the result you would get using the function find_names_in_article
 var sentiment_analysis = function(sentences, data){
     var politicians = {};
     var first = 3;
@@ -874,7 +868,7 @@ var sentiment_analysis = function(sentences, data){
     for (var i = 0; i < names.length; i++){
         var alternate_name = names[i][alt_first] + " " + names[i][alt_last];
         politicians[names[i][last]] = new Politician(names[i][first], names[i][last], alternate_name);
-        }
+    }
 
     //Now using this dictionary to feed all the sentences related to him
     var person = "";
@@ -882,32 +876,32 @@ var sentiment_analysis = function(sentences, data){
     // console.log(references);
     // console.log(1);
     for (var j = 0; j < sentences.length; j++){
-    	for (var s = 0; s < references.length; s++){
-    		if (is_name_in_string(sentences[j], references[s][0] + " " + references[s][1])){
-    			person = references[s][1]
-    		};
-    	};
-    	// console.log(person)
-    	if (person != ""){
-    	if (is_pronoun_in_string(sentences[j], person)){
-    		politicians[person].statements.push(sentences[j]);
-    	};
+        for (var s = 0; s < references.length; s++){
+            if (is_name_in_string(sentences[j], references[s][0] + " " + references[s][1])){
+                person = references[s][1]
+            };
+        };
+        // console.log(person)
+        if (person != ""){
+            if (is_pronoun_in_string(sentences[j], person)){
+                politicians[person].statements.push(sentences[j]);
+            };
+        };
     };
-};
     
     return politicians
-    };
+};
 
 
 
 var article = get_article(example_text);
-var names = find_names_in_article_mod(article);
+var names = find_names_in_article(article);
 var sentences = get_sentences(article);
 sentiment_analysis(sentences, names)
 
 
 function is_pronoun_in_string(paragraph_string, last){
-	var criteria = "/he|He|She|she|" + last + "/"
+    var criteria = "/he|He|She|she|" + last + "/"
     var pattern = new RegExp(criteria);
     var is_in_string = pattern.test(paragraph_string);
     return is_in_string;
