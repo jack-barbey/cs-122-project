af153387f0721a07a864876d9aa7fd1465b8e60b
Eddie Park <ebspark1994@cs.uchicago.edu>
2017-03-02 23:24:15 -0600

    better color schemes for the circles

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -12,19 +12,19 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
 			//Changing the color of the fk circle depending on the score
 			var fk_circle = document.getElementById("fk");
-			if (fk_score >= 70){fk_circle.style.backgroundColor = "#09DC4F"}
-			else if (fk_score >= 50 && fk_score < 70){fk_circle.style.backgroundColor = "#FEF600"}
-			else {fk_circle.style.backgroundColor = "red"};
+			if (fk_score >= 70){fk_circle.style.backgroundColor = "#90FBB7"}
+			else if (fk_score >= 50 && fk_score < 70){fk_circle.style.backgroundColor = "#45E17E"}
+			else {fk_circle.style.backgroundColor = "#0FCA53"};
 
 			//Changing the color of the bias circle depending on the score
 			var bias_circle = document.getElementById("bias");
-			if (bias_score == 0){bias_circle.style.backgroundColor = 'white';}
-			else if (bias_score < 0 && bias_score >= -10) {bias_circle.style.backgroundColor = '#B5B5FE';}
-			else if (bias_score < -10 && bias_score >= -20) {bias_circle.style.backgroundColor = '#7070FE';}
-			else if (bias_score < -20){bias_circle.style.backgroundColor = 'blue';}
-			else if (bias_score > 0 && bias_score <= 10){bias_circle.style.backgroundColor = '#FAB5B5';}
-			else if (bias_score > 10 && bias_score <= 20){bias_circle.style.backgroundColor = '#FB7171';}
-			else {bias_circle.style.backgroundColor = "red";};
+			if (bias_score == 0){bias_circle.style.backgroundColor = '#B9B9B9';}
+			else if (bias_score < 0 && bias_score >= -10) {bias_circle.style.backgroundColor = '#CFCAFE';}
+			else if (bias_score < -10 && bias_score >= -20) {bias_circle.style.backgroundColor = '#9A8FFE';}
+			else if (bias_score < -20){bias_circle.style.backgroundColor = '#6656F9';}
+			else if (bias_score > 0 && bias_score <= 10){bias_circle.style.backgroundColor = '#FEB6B6';}
+			else if (bias_score > 10 && bias_score <= 20){bias_circle.style.backgroundColor = '#F67373';}
+			else {bias_circle.style.backgroundColor = "#FE3636";};
 		});
 });
 	
