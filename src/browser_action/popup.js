28f5f81347f49816c6a6a12a17fa7136a7e6cb6e
Eddie Park <ebspark1994@cs.uchicago.edu>
2017-03-07 19:23:02 -0600

    went through the codes and deleted unnecessary codes

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -6,45 +6,47 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 			var fk_score = data.fk_score;
 			var fk_text = data.fk_text;
 
-			document.getElementById("bias").innerHTML = bias_score;
-			document.getElementById("fk").innerHTML = fk_score;
+			var fk_circle = document.getElementById("fk");
+			var bias_circle = document.getElementById("bias");
+
+			bias_circle.innerHTML = bias_score;
+			fk_circle.innerHTML = fk_score;
 			document.getElementById("fk_text").innerHTML = "- " + fk_text;
 
-			//Changing the color of the fk circle depending on the score
-			var fk_circle = document.getElementById("fk");
-			if (fk_score >= 70){fk_circle.style.backgroundColor = "#90FBB7"}
-			else if (fk_score >= 50 && fk_score < 70){fk_circle.style.backgroundColor = "#45E17E"}
+			// Changing the color of the fk circle depending on the partisanship score
+			if (fk_score >= 70) {fk_circle.style.backgroundColor = "#90FBB7"}
+			else if (fk_score >= 50 && fk_score < 70) {fk_circle.style.backgroundColor = "#45E17E"}
 			else {fk_circle.style.backgroundColor = "#0FCA53"};
 
-			//Changing the color of the bias circle depending on the score
-			var bias_circle = document.getElementById("bias");
-			if (bias_score == 0){bias_circle.style.backgroundColor = '#B9B9B9';}
+			// Changing the color of the bias circle depending on the readability score
+			if (bias_score == 0) {bias_circle.style.backgroundColor = '#B9B9B9';}
 			else if (bias_score < 0 && bias_score >= -10) {bias_circle.style.backgroundColor = '#CFCAFE';}
 			else if (bias_score < -10 && bias_score >= -20) {bias_circle.style.backgroundColor = '#9A8FFE';}
-			else if (bias_score < -20){bias_circle.style.backgroundColor = '#6656F9';}
-			else if (bias_score > 0 && bias_score <= 10){bias_circle.style.backgroundColor = '#FEB6B6';}
-			else if (bias_score > 10 && bias_score <= 20){bias_circle.style.backgroundColor = '#F67373';}
+			else if (bias_score < -20) {bias_circle.style.backgroundColor = '#6656F9';}
+			else if (bias_score > 0 && bias_score <= 10) {bias_circle.style.backgroundColor = '#FEB6B6';}
+			else if (bias_score > 10 && bias_score <= 20) {bias_circle.style.backgroundColor = '#F67373';}
 			else {bias_circle.style.backgroundColor = "#FE3636";};
 
-			//Grab the table
+			// Grab the table
 			var table = document.getElementById("Bias_Info");
 
+			// Inputting scores and sentences that have been analyzed to calculate the partisanship score
 			for (var k = 0; k < top_five.length; k ++){
-				politician = top_five[k][0]
-				console.log(politician)
-				dw_nom = politician.score
-				pol_name = politician.last
-				sentence_score = top_five[k][2]
-				sentence = top_five[k][1]
+				politician = top_five[k][0];
+				dw_nom = politician.score;
+				pol_name = politician.last;
+				sentence_score = top_five[k][2];
+				sentence = top_five[k][1];
 
 				// Bolds the name of the politician in question
 				sentence = sentence.replace(new RegExp('(\\b)(' + pol_name + ')(\\b)','ig'), '$1<b>$2</b>$3');
 
-
 				var row = table.insertRow(k + 1);
 				var cell_1 = row.insertCell(0);
 				var cell_2 = row.insertCell(1);
-				row.id= "newid";
+				
+				// Assigning a new id to the row tag
+				row.id = "newid";
 
 				// No sentiment
 				if (sentence_score == 0) {cell_1.id = "neutral"; cell_2.id = "neutral";}
@@ -55,8 +57,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 				// Liberal, pos sentiment
 				else if (dw_nom < 0 && sentence_score > 0) {cell_1.id = "liberal"; cell_2.id = "liberal";}
 				// Liberal, neg sentiment
-				else if (dw_nom < 0 && sentence_score < 0) {cell_1.id = "conserv"; cell_2.id = "conserv";}
-
+				else if (dw_nom < 0 && sentence_score < 0) {cell_1.id = "conserv"; cell_2.id = "conserv";};
 
 				cell_1.innerHTML = sentence_score;
 				cell_2.innerHTML = sentence;
--- src/content/content.js
+++ src/content/content.js
@@ -1,31 +1,32 @@
 
 var full_text = document.body.innerText.replace(/(\r\n|\n|\r)/gm,"\\n").replace(/"/gm,"'");
-var packaged_response = GetBias(full_text)
+var packaged_response = GetBias(full_text);
 
-var bias_obj = packaged_response[0]
-var fk_obj = packaged_response[1]
+var bias_obj = packaged_response[0];
+var fk_obj = packaged_response[1];
 
-var fk_score = fk_obj[0]
-var fk_text = fk_obj[1]
+var fk_score = fk_obj[0];
+var fk_text = fk_obj[1];
 
-var bias_score = bias_obj[0]
-var observations = bias_obj[1]
-var top_five = bias_obj[2]
+var bias_score = bias_obj[0];
+var observations = bias_obj[1];
+var top_five = bias_obj[2];
 
 var leng = top_five.length;
 for (var i = 0; i < leng; i++) {
   console.log(top_five[i][2], top_five[i][1])
 };
-console.log(bias_score)
-console.log(fk_score)
-console.log(fk_text)
+
+console.log(bias_score);
+console.log(fk_score);
+console.log(fk_text);
 
 
-var data = { bias_score: bias_score,
-						 top_five: top_five,
-						 fk_score: fk_score,
-						 fk_text: fk_text
-						}
+var data = {bias_score: bias_score,
+			top_five: top_five,
+			fk_score: fk_score,
+			fk_text: fk_text
+};
 
 chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
@@ -38,4 +39,4 @@ chrome.runtime.onMessage.addListener(
                 console.error("Unrecognised message: ", message);
         }
     }
-);
+)
